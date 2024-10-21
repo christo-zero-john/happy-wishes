054910787c5372e1b25256312fdbd1e55938a48b
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-21 15:06:19 +0530

    Found modified or deleted file,  src/Components/wishing-page/cecilia-mary-babu/render-wishes.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/Components/wishing-page/cecilia-mary-babu/render-wishes.jsx
+++ src/Components/wishing-page/cecilia-mary-babu/render-wishes.jsx
@@ -3,17 +3,23 @@ import getAllWishes from "../../../modules/firestore/get-all-wishes";
 import { useEffect, useState } from "react";
 
 function RenderWishes(props) {
-  let { renderProfilePic } = props;
+  let { renderProfilePic, setMessage } = props;
   const [wishes, setWishes] = useState([]);
 
   useEffect(() => {
     async function fetchWishes() {
       let fetchedWishes = await getAllWishes();
-      console.log(fetchedWishes);
+      // console.log(fetchedWishes);
       setWishes(fetchedWishes);
     }
-    fetchWishes();
-  },[]);
+    fetchWishes().then(() => {
+      console.log("Wishes Fetched successfully");
+      setMessage("Connected and Everything Perfect.");
+      setTimeout(() => {
+        document.getElementById("homePageLoader").remove();
+      }, 1000);
+    });
+  }, []);
 
   return wishes.map((wish, index) => {
     return (
