6c763387a36ec0d2b986d12e5a5b1036106fdecf
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-21 08:08:20 +0530

    Found new file,  src/AppRoutes.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HappyWishes from "./Components/HappyWishes";
+import NewWishingPage from "./Components/wishing-page/new-wishing-page";
+import MyWishingPages from "./Components/wishing-page/my-pages";
+import EditWishingPage from "./Components/wishing-page/edit-wishing-page";
+import CeciliaMaryBabu from "./Components/wishing-page/cecilia-mary-babu/cecilia-mary-babu";
+
+function AppRoutes() {
+  return (
+    <div className="en-Oxanium">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HappyWishes />} />
+          <Route path="/create/wishing" element={<NewWishingPage />} />
+          <Route path="/wishing/my-pages" element={<MyWishingPages />} />
+          <Route
+            path="/wishing/my-pages/edit/:id"
+            element={<EditWishingPage />}
+          />
+          <Route
+            path="/wishme/cecilia-mary-babu"
+            element={<CeciliaMaryBabu />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default AppRoutes;
