50ecc701f25ad8b8d4291be3fbbf2b809ba0528f
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-22 17:37:25 +0530

    Found modified or deleted file,  src/Components/HappyWishes.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/Components/HappyWishes.jsx
+++ src/Components/HappyWishes.jsx
@@ -1,15 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 function HappyWishes() {
-  window.location.href = "/wishme/cecilia-mary-babu";
-  return (
-    <div>
-      <Link to="/wishing/new "> New Wishing Page</Link>
-      <Link to="/wishing/my-pages "> My Pages</Link>
-    </div>
-  );
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/wishme/cecilia-mary-babu");
+  }, [navigate]);
+
+  return null; // or you could return a loading indicator here
 }
 
 export default HappyWishes;
