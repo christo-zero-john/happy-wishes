7124616a894298ea9c884a8c6b3eb48dd880d1f5
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-22 07:17:03 +0530

    Update HappyWishes.jsx

--- src/Components/HappyWishes.jsx
+++ src/Components/HappyWishes.jsx
@@ -3,10 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 function HappyWishes() {
-  return <div>
-    <Link to="/wishing/new "> New Wishing Page</Link>
-    <Link to="/wishing/my-pages "> My Pages</Link>
-  </div>;
+  window.location.href = "/wishme/cecilia-mary-babu";
+  return (
+    <div>
+      <Link to="/wishing/new "> New Wishing Page</Link>
+      <Link to="/wishing/my-pages "> My Pages</Link>
+    </div>
+  );
 }
 
 export default HappyWishes;
