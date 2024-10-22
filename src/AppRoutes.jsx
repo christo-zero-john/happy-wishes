a1337ac9cdb652a502dd60d95b27a0bb4bf3d974
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-22 17:37:24 +0530

    Found modified or deleted file,  src/AppRoutes.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HappyWishes from "./Components/HappyWishes";
 import NewWishingPage from "./Components/wishing-page/new-wishing-page";
 import MyWishingPages from "./Components/wishing-page/my-pages";
@@ -14,16 +14,18 @@ function AppRoutes() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HappyWishes />} />
+          <Route
+            path="/wishme/cecilia-mary-babu/"
+            element={<CeciliaMaryBabu />}
+          />
           {/* <Route path="/create/wishing" element={<NewWishingPage />} /> */}
           {/* <Route path="/wishing/my-pages" element={<MyWishingPages />} /> */}
           {/* <Route             // path="/wishing/my-pages/edit/:id"
             // element={<EditWishingPage />}
           // />*/}
-          <Route
-            path="/wishme/cecilia-mary-babu/"
-            element={<CeciliaMaryBabu />}
-          />
           {/* <Route path="/hpl" element={<HomePageLoading message="Loading..."/>} /> */}
+          {/* Catch-all route for any unmatched paths */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </div>
