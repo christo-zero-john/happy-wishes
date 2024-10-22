fb0e452ee5d80354d5b729bc3ee63c9ea4683e35
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-22 18:15:22 +0530

    Found modified or deleted file,  src/Components/wishing-page/cecilia-mary-babu/render-pics.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/Components/wishing-page/cecilia-mary-babu/render-pics.jsx
+++ src/Components/wishing-page/cecilia-mary-babu/render-pics.jsx
@@ -1,78 +1,15 @@
 import { boyPics, girlPics } from "./profile-pic-imports";
 
-function renderBoyPics(updateProfilePic) {
-  return boyPics.map((pic, index) => {
-    return (
-      <img
-        className="profile-pic m-2 border rounded"
-        key={index}
-        src={pic}
-        alt="This will be your profile picture"
-        onClick={() => {
-          updateProfilePic("boy," + index);
-        }}
-      />
-    );
-  });
+function renderBoyPics() {
+  return boyPics;
 }
 
-function renderGirlPics(updateProfilePic) {
-  return girlPics.map((pic, index) => {
-    return (
-      <img
-        className="profile-pic m-2 border rounded"
-        key={index}
-        src={pic}
-        alt="This will be your profile picture"
-        onClick={() => {
-          updateProfilePic("girl," + index);
-        }}
-      />
-    );
-  });
+function renderGirlPics() {
+  return girlPics;
 }
 
-function renderProfilePic(picId) {
-  if (picId !== "") {
-    let parts = picId.split(",");
-    if (parts[0] === "boy") {
-      // console.log("Boy Pic Found");
-      let index = +parts[1];
-      return (
-        <img
-          src={boyPics[index]}
-          alt="Select one picture from below"
-          className="d-inline profile-pic"
-        />
-      );
-    } else if (parts[0] === "girl") {
-      // console.log("Girl Pic Found");
-      let index = +parts[1];
-      return (
-        <img
-          src={girlPics[index]}
-          alt="Select one picture from below"
-          className=" d-inline profile-pic"
-        />
-      );
-    } else {
-      console.log("Neither girl nor boy found. Ultimate error detected");
-      return (
-        <p className="text-danger small m-3 d-inline">
-          Choose a Picture from below
-        </p>
-      );
-    }
-  } else {
-    console.log(
-      "Neither girl nor boy found. Ultimate error detected; No splitting performed"
-    );
-    return (
-      <p className="text-danger small m-3 d-inline">
-        Choose a Picture from below
-      </p>
-    );
-  }
+function renderProfilePic(src) {
+  return <img src={src} alt="Profile" className="profile-pic" />;
 }
 
 export { renderBoyPics, renderGirlPics, renderProfilePic };
