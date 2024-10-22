84087a08503452c18a0328f72b7e907e8b3b66f6
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-22 18:25:05 +0530

    Found modified or deleted file,  src/Components/wishing-page/cecilia-mary-babu/wish-item-card.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/Components/wishing-page/cecilia-mary-babu/wish-item-card.jsx
+++ src/Components/wishing-page/cecilia-mary-babu/wish-item-card.jsx
@@ -1,18 +1,13 @@
+import React from "react";
 import "../../../styles/wish-item-card.css";
-import getRandomAOSEffect from "../../../modules/animate-on-scroll/get-random-effect";
-import aosInit from "../../../modules/animate-on-scroll/aos-init";
-import { useEffect } from "react";
 
 function WishItemcard(props) {
   let { renderProfilePic, wish } = props;
-  useEffect(() => {
-    aosInit();
-  }, []);
 
   return (
     <div className="wish-item-card h-100">
       <div className="wish-item-card-header">
-        {renderProfilePic(wish.profilePic)}
+        <img src={wish.profilePic} alt="Profile" className="profile-pic" />
         <h2 className="wish-item-card-title">{wish.creator}</h2>
       </div>
       <div className="wish-item-card-content">
