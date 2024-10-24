a709f072fa3668984afeea9b412748fbeaa5bf99
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-24 20:43:43 +0530

    Found modified or deleted file,  src/AppRoutes.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,7 +3,7 @@ import HappyWishes from "./Components/HappyWishes";
 import NewWishingPage from "./Components/wishing-page/new-wishing-page";
 import MyWishingPages from "./Components/wishing-page/my-pages";
 import EditWishingPage from "./Components/wishing-page/edit-wishing-page";
-import CeciliaMaryBabu from "./Components/wishing-page/cecilia-mary-babu/cecilia-mary-babu";
+import CeciliaMaryBabu from "./Components/wishing-page/cecilia-mary-babu--deleted folder/cecilia-mary-babu";
 import TextEffectParticles from "./Components/text-effects/text-effect-particles";
 import TextEffectGlow from "./Components/text-effects/text-effect-glow";
 import HomePageLoading from "./Components/loading/home-page-loading";
@@ -14,10 +14,10 @@ function AppRoutes() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HappyWishes />} />
-          <Route
+          {/* <Route
             path="/wishme/cecilia-mary-babu/"
             element={<CeciliaMaryBabu />}
-          />
+          /> */}
           {/* <Route path="/create/wishing" element={<NewWishingPage />} /> */}
           {/* <Route path="/wishing/my-pages" element={<MyWishingPages />} /> */}
           {/* <Route             // path="/wishing/my-pages/edit/:id"
