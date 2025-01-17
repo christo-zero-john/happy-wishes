c33e45ffc757ec58c05ed026133169311f637c41
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-01-17 19:42:29 +0530

    Update HappyWishes.jsx

--- src/Components/HappyWishes.jsx
+++ src/Components/HappyWishes.jsx
@@ -1,6 +1,3 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
 function HappyWishes() {
   return <div className="">Will be coming soon</div>; // or you could return a loading indicator here
 }
