c837ae65045b26322890a0d0011e47768463433c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-26 08:36:57 +0530

    Found modified or deleted file,  src/modules/localStore/saveWish--depricated.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/modules/localStore/saveWish--depricated.js
+++
@@ -1,8 +0,0 @@
-function saveWish(newWish) {
-  let wishes = JSON.parse(localStorage.getItem("wishes"));
-  wishes.push(newWish);
-  
-  localStorage.setItem("wishes", JSON.stringify(wishes));
-}
-
-export default saveWish;
