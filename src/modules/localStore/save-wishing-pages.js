203ac48b4fa2b0c0b62bb2145172876b1368638d
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-02-26 08:36:57 +0530

    Found modified or deleted file,  src/modules/localStore/save-wishing-pages.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/modules/localStore/save-wishing-pages.js
+++
@@ -1,6 +0,0 @@
-function saveWishingPages(pages) {
-  localStorage.setItem("wishing-pages", JSON.stringify(pages));
-  console.log("Saved wishing pages successfully");
-}
-
-export default saveWishingPages;
